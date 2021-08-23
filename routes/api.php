74e45b2bba26996a998ec212fea497fc5d0947a2
Max VanReynegom <maxvrdev@gmail.com>
2021-08-23 11:12:34 -0400

    API version-1 routes

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,19 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+/*
+|--------------------------------------------------------------------------
+| Versioned API Routes
+|--------------------------------------------------------------------------
+*/
+
+Route::group([
+    'prefix' => 'v1',
+    'namespace' => 'App\Http\Controllers\Api\V1',
+    'as' => 'api.v1.',
+    'middleware' => ['auth:api'],
+], function () {
+    return require __DIR__ . '/api/v1.php';
+});
