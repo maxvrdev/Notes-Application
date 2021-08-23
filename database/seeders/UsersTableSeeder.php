7a0b9dc3c641ef028d28d3668cb7a4f15eb59262
Max VanRay <max.vanray@exentago.com>
2021-08-23 01:46:24 -0400

    Hydrate the Users table

---
+++ database/seeders/UsersTableSeeder.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\User;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $faker = Faker\Factory::create();
+        $users = [];
+
+        for ($x = 1; $x <= 5; $x++) {
+            $users[$faker->name] = "user{$x}@usernotes.test";
+        }
+
+        foreach ($users as $k => $v) {
+            $user = new User;
+            $user->name = $k;
+            $user->email = $v;
+            $user->password = bcrypt('12345678');
+            $user->save();
+        }
+    }
+}
