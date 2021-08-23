b9dd073848dc0ae4fea6fb629c3055a9c9a485b7
Max VanReynegom <maxvrdev@gmail.com>
2021-08-23 16:15:32 -0400

    Add the rest of the Notes routes

--- routes/api/v1.php
+++ routes/api/v1.php
@@ -7,3 +7,6 @@
 
 Route::get('notes', 'NoteController@index');
 Route::get('notes/{note}', 'NoteController@show');
+Route::post('notes', 'NoteController@store');
+Route::put('notes/{note}/update', 'NoteController@update');
+Route::delete('notes/{note}', 'NoteController@destroy');
